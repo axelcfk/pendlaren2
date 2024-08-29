008a53a24616ff938c8d79d27ededc2dea8805f1
maek95 <mekstrom01@gmail.com>
2024-08-29 16:13:02 +0200

    search input to find matching list of stations, siteInfo not working atm

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,58 @@
-
-
 import express from 'express';
 import cors from 'cors';
+import bodyParser from "body-parser";
+
+
 const app = express();
 
 // Enable CORS for all routes
 app.use(cors());
+// Enable req.body in endpoints..?
+app.use(bodyParser.json());
+
 
 // Using the server-side proxy approach with Express, as shown in the examples, should solve the CORS issues without needing to modify any CORS settings directly in the browser or frontend.
 
 const api_key = "1865697a58b84a44b469a378afb64499";
 
-app.get('/searchStation', async (req, res) => {
+app.post('/searchStation', async (req, res) => {
+
+  const { searchString } = req.body;
+  //const searchString = "Oden"; 
+
   try {
-    const apiResponse = await fetch(`https://journeyplanner.integration.sl.se/v1/typeahead.json?searchstring=Oden&stationsonly=true&maxresults=5&key=${api_key}`);
+    const apiResponse = await fetch(`https://journeyplanner.integration.sl.se/v1/typeahead.json?searchstring=${searchString}&stationsonly=true&maxresults=5&key=${api_key}`);
     const data = await apiResponse.json();
+    console.log(data);
     res.json(data);
+    
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred' });
+    res.status(500).json({ error: 'An error occurred in /searchStation' });
   }
 });
+
+// https://www.trafiklab.se/api/trafiklab-apis/sl/transport/
+// site vs station?
+app.post('/siteInformation', async (req, res) => {
+ // const siteId = 9192; // slussen
+
+  const { siteId } = req.body;
+
+  console.log("finding site information for siteId", siteId);
+  
+
+  try {
+    const apiResponse = await fetch (`https://transport.integration.sl.se/v1/sites/${siteId}/departures
+    `)
+    const data = await apiResponse.json();
+
+    console.log(data);
+    
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred in /stationDepartures' });
+    
+  }
+})
 
 app.listen(3001, () => console.log('Server running on http://localhost:3001'));
--- frontend/src/app/Specificstopdepartures.jsx
+++ frontend/src/app/Specificstopdepartures.jsx
@@ -1,36 +1,135 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api_key } from "./page";
 
-export default function SpecificStopDepartures({stopId}) {
+export default function SpecificStopDepartures({ stopId }) {
+  const [inputSearchStation, setInputSearchStation] = useState("");
+  const [stationsResult, setStationsResult] = useState([]);
+  const [selectedStation, setSelectedStation] = useState(null); // SiteId
+  const [siteInformation, setSiteInformation] = useState(null); // object
 
-  useEffect(() => {
+  /*   useEffect(() => {
     searchStop();
-  }, [])
+  }, []) */
 
-  async function searchStop() {
-
+  async function searchStation(searchString) {
     try {
       //const response = await fetch(`https://journeyplanner.integration.sl.se/v1/typeahead.json?searchstring=Oden&stationsonly=true&maxresults=5&key=${api_key}`)
-      const response = await fetch(`http://localhost:3001/searchStation`)
+      // const response = await fetch(`http://localhost:3001/searchStation`) // GET
+      const response = await fetch(`http://localhost:3001/searchStation`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          searchString: searchString,
+        }),
+      });
 
       const data = await response.json();
 
       console.log(data);
-      
+
+      if (data.ResponseData) {
+        /* note: first letter is always capitalized in this API */
+        setStationsResult(data.ResponseData);
+      } else {
+        console.log(
+          "searchStation fetch data does not contain responseData array?"
+        );
+      }
     } catch (error) {
-      console.error("Error in getStopDepartues():", error);
-      
+      console.error("Error in searchStation():", error);
     }
-
   }
 
- /*  async function name(params) {
-    
+  // currently this only finds data.departures && data.stop_deviations that are empty?
+  // not correct id? Slussen works with siteId 9192? But odenplan 300101079 does not? 
+  async function fetchStationInformation(siteId) {
+    try {
+      const response = await fetch(`http://localhost:3001/siteInformation`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          siteId: siteId,
+        }),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if (data.departures && data.stop_deviations) {
+        setSiteInformation(data);
+      } else {
+        console.log("missing data in siteInformation?");
+      }
+    } catch (error) {
+      console.error("Error in fetchStationInformation():", error);
+    }
   }
- */
+
   return (
     <>
-
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          searchStation(inputSearchStation);
+        }}
+      >
+        <label htmlFor="searchStationName"></label>
+        <input
+          required
+          onChange={(e) => {
+            setInputSearchStation(e.target.value);
+          }}
+          type="text"
+          name=""
+          id="searchStationName"
+        />
+        <button type="submit">Search Station</button>
+      </form>
+      <ul>
+        {stationsResult.length > 0 &&
+          stationsResult.map((station, index) => {
+            /* automatically generated id from map function */
+            return (
+              <li key={index}>
+                {station.Name}
+                <button
+                  onClick={() => {
+                    setSelectedStation(station.SiteId);
+                  }}
+                >
+                  Select
+                </button>
+              </li>
+            );
+          })}
+      </ul>
+      <div>
+        {selectedStation && (
+          <div>
+            <h2>Departures from {selectedStation}</h2>{" "}
+            {/* id right now, change to name */}
+            <button
+              onClick={() => {
+                fetchStationInformation(selectedStation);
+              }}
+            >
+              Display
+            </button>
+            <div>
+              {siteInformation && siteInformation.departures.length > 0 && (
+                <>
+                  <p>Destination:{siteInformation.departures[0].destination}</p>
+                  <span>
+                    scheduled:{" "}
+                    {siteInformation.departures[0].destination.scheduled}
+                  </span>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
